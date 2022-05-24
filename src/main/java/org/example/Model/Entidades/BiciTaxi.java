5244b6752df6a96f617528187fdbf09123e1b15f
lauritagonza <lauragonzalezrojas2015@gmail.com>
2022-05-24 13:20:19 -0500

    Revert "Update BiciTaxi.java"
    This reverts commit 6a8b90c1f864c27c8f23cbe7bfa3527e9160ac9c.

--- src/main/java/org/example/Model/Entidades/BiciTaxi.java
+++ src/main/java/org/example/Model/Entidades/BiciTaxi.java
@@ -1,20 +1,21 @@
-package org.example.Model.Entidades;
+package Entidades;
 
 public class BiciTaxi extends Vehiculo{
     private int rinCicla;
+
+    //Setters y Getters
+
+    int getrin() {
+        return rinCicla;
+    }
+    void setrin(int rin) {
+        this.rinCicla=rin;
+    }
 
     //Constructor
     public BiciTaxi (int rinCicla, int idVehiculo, int idConductor, String Placa, boolean Tipo, int codigoSoat) {
         // TODO Auto-generated constructor stub
         super(idVehiculo, idConductor, Placa, Tipo, codigoSoat);
         this.rinCicla = rinCicla;
-    }
-    //Setters y Getters
-
-    public int getRin() {
-        return rinCicla;
-    }
-    public void setRin(int rin) {
-        this.rinCicla=rin;
     }
 }
