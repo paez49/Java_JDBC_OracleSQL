8570b20ac05bf7f819c987b02654ad55ff565f1c
lauritagonza <105530354+lauritagonza@users.noreply.github.com>
2022-05-24 07:28:43 -0700

    Update Conductor.java

--- src/main/java/org/example/Model/Entidades/Conductor.java
+++ src/main/java/org/example/Model/Entidades/Conductor.java
@@ -1,4 +1,4 @@
-package Entidades;
+package org.example.Model.Entidades;
 
 public class Conductor {
     private int idConductor;
@@ -7,47 +7,10 @@ public class Conductor {
     private int codigoLicencia;
     private String tipoLicencia;
     private int Puntaje;
-
-    //Setters y Getters
-
-    int getidConductor() {
-        return idConductor;
-    }
-    void setidConductor(int id) {
-        this.idConductor=id;
-    }
-    int getcedula() {
-        return Cedula;
-    }
-    void setcedula(int numero) {
-        this.Cedula=numero;
-    }
-    String getnombre() {
-        return Nombre;
-    }
-    void setnombre(String nombre) {
-        this.Nombre=nombre;
-    }
-    int getcodigoLicencia() {
-        return codigoLicencia;
-    }
-    void setcodigoLicencia(int codigo) {
-        this.codigoLicencia=codigo;
-    }
-    String gettipoLicencia() {
-        return tipoLicencia;
-    }
-    void settipoLicencia(String tipo) {
-        this.tipoLicencia=tipo;
-    }
-    int getpuntaje() {
-        return Puntaje;
-    }
-    void setpuntaje(int puntaje) {
-        this.Puntaje=puntaje;
-    }
-
     //Constructor
+    public Conductor(){
+
+    }
     public Conductor( int idConductor, int Cedula, String Nombre, int codigoLicencia, String tipoLicencia, int Puntaje) {
         this.Cedula=Cedula;
         this.idConductor=idConductor;
@@ -56,5 +19,41 @@ public class Conductor {
         this.tipoLicencia=tipoLicencia;
         this.Puntaje=Puntaje;
     }
-
+    // Setters Y Getters
+        public int getIdConductor() {
+            return idConductor;
+    }
+        public void setIdConductor(int id) {
+            this.idConductor=id;
+    }
+        public int getcedula() {
+            return Cedula;
+    }
+        public void setCedula(int numero) {
+            this.Cedula=numero;
+    }
+        public String getNombre() {
+            return Nombre;
+    }
+        public void setNombre(String nombre) {
+            this.Nombre=nombre;
+    }
+        public int getCodigoLicencia() {
+        return codigoLicencia;
+    }
+        public void setCodigoLicencia(int codigo) {
+        this.codigoLicencia=codigo;
+    }
+        public String getTipoLicencia() {
+        return tipoLicencia;
+    }
+        public void setTipoLicencia(String tipo) {
+        this.tipoLicencia=tipo;
+    }
+        public int getPuntaje() {
+        return Puntaje;
+    }
+        public void setPuntaje(int puntaje) {
+        this.Puntaje=puntaje;
+    }
 }
