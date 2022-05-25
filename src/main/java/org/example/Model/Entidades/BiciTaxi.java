e121e983da9d06cd6f953a82d4bfafff041bde4d
lauritagonza <105530354+lauritagonza@users.noreply.github.com>
2022-05-24 21:15:40 -0500

    Update BiciTaxi.java

--- src/main/java/org/example/Model/Entidades/BiciTaxi.java
+++ src/main/java/org/example/Model/Entidades/BiciTaxi.java
@@ -1,21 +1,23 @@
-package org.example.Model.Entidades;
+package Entidades;
 
 public class BiciTaxi extends Vehiculo{
     private int rinCicla;
 
-    //Setters y Getters
-
-    int getrin() {
-        return rinCicla;
-    }
-    void setrin(int rin) {
-        this.rinCicla=rin;
-    }
-
-    //Constructor
+        //Constructor
     public BiciTaxi (int rinCicla, int idVehiculo, int idConductor, String Placa, boolean Tipo, int codigoSoat) {
         // TODO Auto-generated constructor stub
         super(idVehiculo, idConductor, Placa, Tipo, codigoSoat);
         this.rinCicla = rinCicla;
     }
+    
+    //Setters y Getters
+
+    int getRin() {
+        return rinCicla;
+    }
+    void setRin(int rin) {
+        this.rinCicla=rin;
+    }
+
+
 }
