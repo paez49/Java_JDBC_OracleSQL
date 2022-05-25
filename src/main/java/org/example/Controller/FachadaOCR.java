0e8eea841fcadcd1a9ad4cbba3706c53d8881257
lauritagonza <105530354+lauritagonza@users.noreply.github.com>
2022-05-25 13:56:51 -0500

    Update FachadaOCR.java

--- src/main/java/org/example/Controller/FachadaOCR.java
+++ src/main/java/org/example/Controller/FachadaOCR.java
@@ -16,4 +16,28 @@ public class FachadaOCR {
         QuerysSITP2 querysSITP2 = new QuerysSITP2();
         return querysSITP2.getConductorCedula(cedula);
     }
+    
+    public void  añadirConductor(int idConductor, int Cedula, String Nombre, int codigoLicencia, String tipoLicencia, int Puntaje){
+
+        QuerysSITP2 queryInsertCon = new QuerysSITP2();
+        queryInsertCon.InsertarConductor(idConductor, Cedula, Nombre, codigoLicencia, tipoLicencia, Puntaje);
+        System.out.println("Fue ingresado exitosamente");
+    }
+
+    public void modificarConductor(int Cedula, String Nombre, int codigoLicencia, String tipoLicencia, int Puntaje){
+
+        List<Conductor> lista= new List<Conductor>
+        QuerysSITP2 queryModifyCon = new QuerysSITP2();
+        lista=queryModifyCon.getConductorCedula(Cedula); 
+        int idConductor= lista.get(0); 
+        
+        queryModifyCon.ModificarConductor(idConductor, Cedula, Nombre, codigoLicencia, tipoLicencia, Puntaje);
+        System.out.println("Fue modificado exitosamente");
+    }
+    public void  eliminarConductor(int idConductor){
+
+        QuerysSITP2 queryDeleteCon = new QuerysSITP2();
+        queryDeleteCon.deleteConductor(idConductor);
+        System.out.println("Fue eliminado exitosamente");
+    }
 }
